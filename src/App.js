e0331c065db753c3466716fe0f7d2d3f3870e86b
Burhanuddin Marcha <75224524+Burhanuddin-49@users.noreply.github.com>
2022-01-07 19:11:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+const url = "https://type.fit/api/quotes";
+let data;
+const randomNo = () => Math.floor(Math.random() * data.length) + 1;
+
+export default function App() {
+  const [quotes, setQuotes] = useState({});
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  async function getQuotes() {
+    try {
+      const res = await fetch(url);
+      data = await res.json();
+      setQuotes(data[randomNo()]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return (
+    <div className="App">
+      <h1 className="title">Quotes Generator</h1>
+      <p className="quotes">{quotes.text}</p>
+      <p className="author">- {quotes.author ? quotes.author : "Anonymous"}</p>
+      <button className="button" onClick={getQuotes}>
+        New Quote
+      </button>
+    </div>
+  );
+}
